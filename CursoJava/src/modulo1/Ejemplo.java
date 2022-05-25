3b8f82daa69e730ee7f78832101c126d09f018be
jordi-donadac <jordi.donadac@allianz.es>
2022-05-25 14:21:59 +0200

    ejercicio1

---
+++ CursoJava/src/modulo1/Ejemplo.java
@@ -0,0 +1,13 @@
+package modulo1;
+
+public class Ejemplo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//sysout + Control + space
+		System.out.println("Hola mundo");
+		System.out.println("\nnombre\tapellido\tlink git");
+		System.out.println();
+	}
+
+}
