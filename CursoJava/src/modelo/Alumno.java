85a286d676434b651fd70dcc09fbf8f383b1d330
jordi-donadac <jordi.donadac@allianz.es>
2022-05-30 16:31:56 +0200

    nuevo paquete modelo

---
+++ CursoJava/src/modelo/Alumno.java
@@ -0,0 +1,33 @@
+package modelo;
+
+public class Alumno extends Persona {
+	int legajo;
+
+	public Alumno() {
+		legajo = 100;
+	}
+
+	public Alumno(int legajo) {
+		super();
+		this.legajo = legajo;
+	}
+	
+	public Alumno(String pNom, String pApe, int pLegajo) {
+		super(pNom, pApe);
+		this.legajo = pLegajo;
+	}
+
+	public int getLegajo() {
+		return legajo;
+	}
+
+	public void setLegajo(int legajo) {
+		this.legajo = legajo;
+	}
+
+	@Override
+	public String toString() {
+		return "Alumno [legajo=" + legajo + "]";
+	}
+	
+}
---
+++ CursoJava/src/modelo/Persona.java
@@ -0,0 +1,70 @@
+package modelo;
+
+public class Persona {
+	
+	private String apellido;
+	private String nombre;
+
+	public Persona(){
+		apellido = "Garcia";
+		nombre   = "Jose";
+	}
+
+	public Persona(String pNom, String pApe){
+		nombre   = pNom;
+		apellido = pApe;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((apellido == null) ? 0 : apellido.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Persona))
+			return false;
+		Persona other = (Persona) obj;
+		if (apellido == null) {
+			if (other.apellido != null)
+				return false;
+		} else if (!apellido.equals(other.apellido))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [apellido=" + apellido + ", nombre=" + nombre + "]";
+	}	
+
+}
