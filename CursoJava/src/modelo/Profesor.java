5a3cf79b2a04acb814f4d8203d5f5b07c5f9115d
jordi-donadac <jordi.donadac@allianz.es>
2022-05-31 10:56:38 +0200

    nueva clase Profesor

---
+++ CursoJava/src/modelo/Profesor.java
@@ -0,0 +1,62 @@
+package modelo;
+
+public class Profesor extends Persona {
+	
+	String iosfa;
+
+	public Profesor() {
+		super();
+		iosfa = " ";
+	}
+		
+	public Profesor(String pIo) {
+		super();
+		iosfa = pIo;
+	}
+
+	public Profesor(String pNom, String pApe, String pIo) {
+		super(pNom, pApe);
+		iosfa = pIo; 
+	}
+
+	public String getIosfa() {
+		return iosfa;
+	}
+
+	public void setIosfa(String pIo) {
+		iosfa = pIo;
+	}
+
+	@Override
+	public String toString() {
+		return "Profesor [iosfa=" + iosfa + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((iosfa == null) ? 0 : iosfa.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (!(obj instanceof Profesor))
+			return false;
+		Profesor other = (Profesor) obj;
+		if (iosfa == null) {
+			if (other.iosfa != null)
+				return false;
+		} else if (!iosfa.equals(other.iosfa))
+			return false;
+		return true;
+	}
+	
+	
+
+}
